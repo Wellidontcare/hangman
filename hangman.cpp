866300e3e7cd6cd4fd1f5dd8408a852deebb928f
Wellidontcare <djjorisdj@gmail.com>
2021-02-12 11:35:59 +0100

    modernize code
    - fix a bug where the game would let you input characters after the was
      over
    - remove global file paths
    - restructure code

--- hangman.cpp
+++ hangman.cpp
@@ -6,19 +6,18 @@
 /*  By: Wellidontcare <djjorisdj@gmail.com>                    */
 /*                                                             */
 /*  created: 08/01/20 10:37:47 by Wellidontcare                */
-/*  updated: 09/10/20 09:48:56 by Joris Nonnast                */
+/*  updated: 09/10/20 10:05:06 by Joris Nonnast                */
 /*                                                             */
 /*                                                             */
 /* **********************************************************²**/
-#include <fstream>
 #include <array>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-#include <set>
 #include <random>
+#include <set>
 #include <string>
-
-const std::string stages_file_path = "../hangman_stages.lst";
-const std::string words_file_path = "../words.lst";
+#include <string_view>
 
 struct Point {
     unsigned x;
@@ -31,11 +30,11 @@ class Canvas{
     std::array<char, width*height> buffer_;
 public:
     Canvas() : buffer_() {
-        unsigned pos_counter = 0;
-        std::generate(buffer_.begin(), buffer_.end(), [&pos_counter](){
-            pos_counter++;
-            return pos_counter % width == 0 ? '\n' : ' ';
-        });
+      auto pos_counter = 0u;
+      std::generate(buffer_.begin(), buffer_.end(), [&pos_counter]() {
+        pos_counter++;
+        return pos_counter % width == 0 ? '\n' : ' ';
+      });
     }
 
     void add_text(std::string text, Point point){
@@ -44,8 +43,8 @@ public:
 
     void show() {
         printf("\033c");
-        std::string printable_buffer(buffer_.begin(), buffer_.end());
-        printf("%s", printable_buffer.c_str());
+        std::string_view printable_buffer(buffer_.begin(), buffer_.end());
+        std::cout << printable_buffer;
     }
 
     void clear(){
@@ -71,7 +70,7 @@ class HangmanGame{
     std::string current_word_;
 
     bool game_over_ = false;
-    bool game_ends_ = false;
+    bool finished = false;
     unsigned lives_ = 11;
 
     //ui
@@ -83,37 +82,47 @@ class HangmanGame{
     char input_;
 
 public:
-    explicit HangmanGame(const std::string& wordlist_file_path) : input_() {
-        std::ifstream words_file(wordlist_file_path);
-        std::string file_input;
-        while(words_file >> file_input){
-            words_.emplace_back(file_input);
-        }
-        std::ifstream stages_file(stages_file_path);
-        std::vector<std::string> stage;
-        unsigned line_counter = 7;
-        while(std::getline(stages_file, file_input, '\n')){
-            if(line_counter > 0){
-                stage.emplace_back(file_input);
-                line_counter--;
-            }
-            else{
-                line_counter = 7;
-                stage.emplace_back(file_input);
-                stages_.emplace_back(stage);
-                stage.clear();
-            }
-        }
-        stages_.emplace_back(std::vector<std::string>{" ", " ", " ", " ", " ", " ", " ", " "});
-
-        for(auto& word : words_) {
-            std::transform(word.begin(), word.end(), word.begin(), toupper);
-        }
-
-        std::random_device rd;
-        std::mt19937 g(rd());
-        std::shuffle(words_.begin(), words_.end(), g);
+  explicit HangmanGame(const std::string &wordlist_file_path,
+                       const std::string &stages_file_path)
+      : input_() {
+    if (!std::filesystem::exists(wordlist_file_path)) {
+      std::cout << "Words file missing\n";
+      return;
     }
+    if (!std::filesystem::exists(stages_file_path)) {
+      std::cout << "Stages file missing\n";
+      return;
+    }
+    std::ifstream words_file(wordlist_file_path);
+    std::string file_input;
+    while (words_file >> file_input) {
+      words_.emplace_back(file_input);
+    }
+    std::ifstream stages_file(stages_file_path);
+    std::vector<std::string> stage;
+    auto line_counter = 7u;
+    while (std::getline(stages_file, file_input, '\n')) {
+      if (line_counter > 0) {
+        stage.emplace_back(file_input);
+        line_counter--;
+      } else {
+        line_counter = 7;
+        stage.emplace_back(file_input);
+        stages_.emplace_back(stage);
+        stage.clear();
+      }
+    }
+    stages_.emplace_back(
+        std::vector<std::string>{" ", " ", " ", " ", " ", " ", " ", " "});
+
+    for (auto &word : words_) {
+      std::transform(word.begin(), word.end(), word.begin(), toupper);
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(words_.begin(), words_.end(), g);
+  }
     void start_new_game() {
         if(guess_index_ > words_.size()){
             std::random_device rd;
@@ -129,76 +138,79 @@ public:
         lives_ = 11;
         std::copy(current_word_.begin(), current_word_.end(), std::inserter(word_set_, word_set_.begin()));
         word_set_tracker_ = word_set_;
-        game_ends_ = false;
+        finished = false;
         game_over_ = false;
         game_loop();
     }
 
 private:
     void game_loop(){
-        while(!game_ends_) {
-            game_ends_ = game_over_;
-            canvas_.add_text(status_message, status_text_origin_);
-            std::string guess_indicator(current_word_);
-            std::set<char> falsely_guessed_letters;
-            std::set_difference(guessed_letters_.begin(), guessed_letters_.end(), word_set_.begin(), word_set_.end(), std::inserter(falsely_guessed_letters, falsely_guessed_letters.begin()));
-            std::string guessed_letters_str(falsely_guessed_letters.begin(), falsely_guessed_letters.end());
-            std::transform(guess_indicator.begin(), guess_indicator.end(), guess_indicator.begin(),
-                           [this](char c){
-                return guessed_letters_.contains(c) ? c : '_';
-            });
-            canvas_.add_text(guess_indicator, guess_text_origin_);
-            canvas_.add_text(guessed_letters_str, guessed_letters_text_origin_);
-            Point hangman_loc = {width/2, height/2};
-            for(std::string& line : stages_[lives_]){
-                canvas_.add_text(line, hangman_loc);
-                hangman_loc.y++;
-            }
-            canvas_.show();
-            if(!game_over_){
-                printf("\nWhat's your guess?: ");
-            }
-            else{
-                printf("\nAgain? (Y/N)");
-                std::cin >> input_;
-                input_ = std::toupper(input_);
-                if(input_ == 'Y'){
-                    start_new_game();
-                    return;
-                }else{
-                    game_over_ = true;
-                    return;
-                }
-            }
+      while (!finished) {
+        if (!game_over_) {
+          canvas_.add_text(status_message, status_text_origin_);
+          std::string guess_indicator(current_word_);
+          std::set<char> falsely_guessed_letters;
+          std::set_difference(guessed_letters_.begin(), guessed_letters_.end(),
+                              word_set_.begin(), word_set_.end(),
+                              std::inserter(falsely_guessed_letters,
+                                            falsely_guessed_letters.begin()));
+          std::string guessed_letters_str(falsely_guessed_letters.begin(),
+                                          falsely_guessed_letters.end());
+          std::transform(guess_indicator.begin(), guess_indicator.end(),
+                         guess_indicator.begin(), [this](char c) {
+                           return guessed_letters_.contains(c) ? c : '_';
+                         });
+          canvas_.add_text(guess_indicator, guess_text_origin_);
+          canvas_.add_text(guessed_letters_str, guessed_letters_text_origin_);
+          Point hangman_loc = {width / 2, height / 2};
+          for (std::string &line : stages_[lives_]) {
+            canvas_.add_text(line, hangman_loc);
+            hangman_loc.y++;
+          }
+          canvas_.show();
+        }
+
+        if (lives_ < 1 || word_set_tracker_.empty()) {
+          game_over_ = finished = true;
+          if (lives_ < 1) {
+            std::cout << "You lost... The word was " + current_word_ << "\n";
+          } else {
+            std::cout << "Congratulations, you win!\n";
+            finished = true;
+          }
+            std::cout << "\nAgain? (Y/N)";
             std::cin >> input_;
             input_ = std::toupper(input_);
-            if(lives_ < 1) {
-                status_message = "You lost... The word was " + current_word_;
-                game_over_ = true;
-            } else if(guessed_letters_.contains(input_)) {
-                status_message = "You already guessed that letter!";
-                lives_--;
-            } else if(word_set_.contains(input_)) {
-                guessed_letters_.emplace(input_);
-                status_message = "Right!";
-                word_set_tracker_.erase(input_);
-                if (word_set_tracker_.empty()) {
-                    status_message = "You win!";
-                    game_over_ = true;
-                }
+            if (input_ == 'Y') {
+              start_new_game();
+              return;
             } else {
-                guessed_letters_.emplace(input_);
-                status_message = "Wrong!";
-                lives_--;
+              return;
             }
-            canvas_.clear();
+
+            std::cout << "\nWhat's your guess?: ";
+            std::cin >> input_;
+            input_ = std::toupper(input_);
+            if (guessed_letters_.contains(input_)) {
+              status_message = "You already guessed that letter!";
+              lives_--;
+        } else if (word_set_.contains(input_)) {
+          guessed_letters_.emplace(input_);
+          status_message = "Right!";
+          word_set_tracker_.erase(input_);
+        } else {
+          guessed_letters_.emplace(input_);
+          status_message = "Wrong!";
+          lives_--;
         }
+        canvas_.clear();
+      }
     }
 };
 
 
 int main(){
-    HangmanGame<50, 25> game(words_file_path);
-    game.start_new_game();
-    return 0;
+  HangmanGame<50, 25> game("words.lst", "hangman_stages.lst");
+  game.start_new_game();
+  return 0;
 }
